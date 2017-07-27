0439a50216705e48dbd7917544f82bfeb279d6d3
wenhaowuuu <wenhaow@upenn.edu>
2017-07-27 15:54:42 -0400

    0727_6

--- js/js11.js
+++ js/js11.js
@@ -30,6 +30,27 @@ $('.enter1').click(function () {
 
 
 
+//ADD A TIMER
+function startTime() {
+    var today = new Date();
+    var mo = today.getMonth() + 1;
+    var d = today.getDate();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    mo + "-" + d + " " + h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+
+
 
 //the migration is complete.
 // TARGET: TO LEARN THE OSMNX TOOLBOX
