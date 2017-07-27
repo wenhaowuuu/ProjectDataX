88a171943bd5af895a506d31415561502681168f
wenhaowuuu <wenhaow@upenn.edu>
2017-07-27 14:16:20 -0400

    0727_4

--- Leaflet.print-master/Leaflet.print-master/src/copyright.js
+++
@@ -1,4 +0,0 @@
-/*
-	Leaflet.print, implements the Mapfish print protocol allowing a Leaflet map to be printed using either the Mapfish or GeoServer print module.
-	(c) 2013, Adam Ratcliffe, GeoSmart Maps Limited
-*/
